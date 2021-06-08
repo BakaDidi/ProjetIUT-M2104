c883a853c05c74dd7217af3f7229d1b37603546c
Louis <louis.schinkler@gmail.com>
2021-06-08 16:35:49 +0200

    Remplissage du tableau d'unités (Cryptage)

--- RhumGuybrush/Unite.cs
+++ RhumGuybrush/Unite.cs
@@ -6,23 +6,23 @@ namespace RhumGuybrush
 {
     class Unite
     {
-        private uint nb_Frontieres;
+        private uint val_Frontieres;
         private bool plantable;
         private char type_Unite; // M --> Mer | F --> Forêt | T --> Terre
 
-        public Unite(char lettre_Unite, uint nb_Frontieres2)
+        public Unite(char lettre_Unite, uint val_Frontieres2)
         {
-            nb_Frontieres = nb_Frontieres2;
+            val_Frontieres = val_Frontieres2;
 
             if (lettre_Unite == 'M')
             {
-                nb_Frontieres += 64;
+                val_Frontieres += 64;
                 plantable = false;
             }
 
             if (lettre_Unite == 'F')
             {
-                nb_Frontieres += 32;
+                val_Frontieres += 32;
                 plantable = false;
             }
             else
@@ -30,6 +30,12 @@ namespace RhumGuybrush
                 plantable = true;
             }
         }
+
+        public uint Val_Frontieres
+        {
+            get => val_Frontieres;
+        }
+
 
     }
 }
