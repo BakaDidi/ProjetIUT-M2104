b6247d196f10f3f5fc53bf4623fabfea67d5cf80
Louis <louis.schinkler@gmail.com>
2021-06-08 15:26:37 +0200

    Suite

---
+++ RhumGuybrush/Parcelle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhumGuybrush
+{
+    class Parcelle
+    {
+        private char nom;
+        private uint nb_Unites;
+
+        public Parcelle(char nom2)
+        {
+            if (nom2 != 'F' && nom2 != 'M')
+                nom = nom2;
+        }
+
+        public void Parcelle_increment()
+        {
+            nb_Unites++;
+        }
+    }
+}
--- RhumGuybrush/Program.cs
+++ RhumGuybrush/Program.cs
@@ -9,24 +9,27 @@ namespace RhumGuybrush
         {
             char[,] tab1 = new char[10, 10];
             int i, j;
-            for (i = 0; i < 3; i++)
-            {
-                for (j = 0; j < 2; j++)
-                {
-                    Console.WriteLine("tab2D[{0},{1}] = {2}", i, j, tab1[i, j]);
-                }
-            }
-            // Create an instance of StreamReader to read from a file.
-            // The using statement also closes the StreamReader.
             using (StreamReader sr = new StreamReader("file.txt"))
             {
                 string line;
-                // Read and display lines from the file until the end of
-                // the file is reached.
                 while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    for (i = 0; i < 10; i++)
+                    {
+                        for (j = 0; j < 10; j++)
+                        {
+                            tab1[i, j] = line[j]; 
+                        }
+                        line = sr.ReadLine();
+                    }
+                }
 
+                for (i = 0; i < 10; i++)
+                {
+                    for (j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine("tab2D[{0},{1}] = {2}", i, j, tab1[i, j]);
+                    }
                 }
             }
         }
