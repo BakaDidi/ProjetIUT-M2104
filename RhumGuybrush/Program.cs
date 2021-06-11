74c1f9d69689d09e542dd6d809ce4801072b6c89
Antoine Roussel <antoine.roussel138@gmail.com>
2021-06-11 12:52:16 +0200

    Mise à jour de l'affichage.

--- RhumGuybrush/Program.cs
+++ RhumGuybrush/Program.cs
@@ -9,6 +9,9 @@ namespace RhumGuybrush
         {
 
             string reponse;
+            string reponse;
+            string reponse;
+            string reponse;
             do
             {
                 Console.WriteLine("Quel mode voulez vous choisir ? Entrez \"D\" pour le mode déchiffrage et \"C\" pour le chiffrage. Pour quitter, entrez \"Q\"");
@@ -34,6 +37,41 @@ namespace RhumGuybrush
                         Carte.Decryptage(fichier);
                         Console.WriteLine("");
                         Console.WriteLine("");
+                        
+                        Console.WriteLine("Voulez vous afficher la les coordonées et le nombre d'unites de toutes les parcelles ? (Y/N)");
+                        reponse = Console.ReadLine();
+                        if(reponse == "Y")
+                        {
+                                Affiche_Parcelles();
+                        }
+                        Console.WriteLine("Voulez vous afficher la les coordonées et le nombre d'unites d'une parcelles cible ? (Y/N)");
+                        reponse = Console.ReadLine();
+                        if(reponse == "Y")
+                        {
+                                Console.WriteLine("Entrez le nom de la parcelle à cibler");
+                                reponse = Console.ReadLine();
+                                Taille_Parcelles(reponse);
+                        }
+                        Console.WriteLine("Voulez vous afficher les parcelles plus grandes qu'une taille choisi ? (Y/N)");
+                        reponse = Console.ReadLine();
+                        if(reponse == "Y")
+                        {
+                                Console.WriteLine("Entrez la taille minimum des parcelles à cibler");
+                                reponse = Console.ReadLine();
+                                Affiche_List_Parcelles_Sup_Valeur(int valeur);
+                        }
+                        Console.WriteLine("Voulez vous afficher la taille moyenne des parcelles ? (Y/N)");
+                        reponse = Console.ReadLine();
+                        if(reponse == "Y")
+                        {
+                                Moyenne_Taille_Parcelles();
+                        }
+
+
+                     reponse = Console.ReadLine();
+
+                        
+
                     }
 
                     else
